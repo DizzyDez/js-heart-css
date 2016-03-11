a90c5ceb8541d2da61f2b28aa092167e1e8055e3
DizzyDez <desc0137@algonquinlive.com>
2016-03-11 10:31:34 -0500

    final changes and expected content

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
 var $header = $('header');
 var $h1 = $('h1');
 var $main = $('main');
-var $firstP = $('main:first-child');
+var $firstp = $('main p:first-child');
 var $h2 = $('h2');
 var $ul = $('ul');
-var $img = $('img')
+var $img = $('img');
+var $firstlist = $('li:first-child');
+var $lastlist = $('li:last-child');
+var $dl = $('dl');
+var $dt = $('dt');
 
 $header.addClass('masthead');
 $h1.removeClass('big-heading');
 $h1.addClass('logo');
 $main.addClass('wrapper');
-$firstP.addClass('intro');
+$firstp.addClass('intro');
 $h2.addClass('slug-head');
 $ul.addClass('slug-list');
-img.addClass('slug-img');
+$img.addClass('slug-img');
+$firstlist.addClass('slug-list-first');
+$lastlist.addClass('slug-list-last');
+$dl.addClass('classification');
+$dt.removeClass('big-label');
+$lastlist.show();
