e43c3f48341c90e22ef04080d8e76e7b7187eccc
DizzyDez <desc0137@algonquinlive.com>
2016-03-11 09:57:54 -0500

    halfway through js manipulations

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var $header = $('header');
+var $h1 = $('h1');
+var $main = $('main');
+var $firstP = $('main:first-child');
+var $h2 = $('h2');
+var $ul = $('ul');
+var $img = $('img')
+
+$header.addClass('masthead');
+$h1.removeClass('big-heading');
+$h1.addClass('logo');
+$main.addClass('wrapper');
+$firstP.addClass('intro');
+$h2.addClass('slug-head');
+$ul.addClass('slug-list');
+img.addClass('slug-img');
